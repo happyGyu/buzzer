34123fe34d294a4d58e8726cb5a488c86e5e0ecd
happyGyu <happyboyto.dev@gmail.com>
2023-12-18 02:20:22 +0900

    feat: 밀리초까지 클릭 시간 표시

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const resetButton = document.getElementById("reset-button");
 const buzzerSound = document.getElementById("buzzerSound");
 const timeDisplay = document.getElementById("timeDisplay");
 
+function getFormattedCurrentTime() {
+  const now = new Date();
+  const hours = String(now.getHours());
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const milliseconds = String(now.getMilliseconds()).padStart(3, "0");
+  return `${hours}시 ${minutes}분 ${seconds}.${milliseconds}초`;
+}
+
 function handleBuzzerButtonClick() {
   buzzerSound.play();
-  const currentTime = new Date().toLocaleTimeString();
-  timeDisplay.textContent = currentTime;
+  timeDisplay.textContent = getFormattedCurrentTime();
   resetButton.classList.remove("hide");
 }
 
